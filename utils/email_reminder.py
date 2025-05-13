aed7117249098e7c1e91ddbd513e59b9ba0729e4
mac <ahuekweprinceugo@gmail.com>
2025-05-13 21:32:53 +0100

    email reminder fix

--- utils/email_reminder.py
+++ utils/email_reminder.py
@@ -11,9 +11,9 @@ def send_email_reminder(email: str, message: str, subject: str):
     msg["From"] = "ahuekweprinceugo@gmail.com"
     msg["To"] = email
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.ehlo()
-        server.starttls()
-        server.ehlo()
+        # server.starttls()
+        # server.ehlo()
         server.login("ahuekweprinceugo@gmail.com", os.getenv("GMAIL_PASS"))
         server.sendmail(msg["From"], [msg["To"]], msg.as_string())
